60a5a6ca4633db3ce9fec8de156d0b42378efb6b
gc <gc@gc-ThinkPad-T440p>
2020-04-05 16:16:40 +0200

    Parte 1 : manca doSpellCheck

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -5,7 +5,11 @@
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,6 +18,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	private Dictionary dizionario;
+	private List<String> inputTextList;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -44,17 +51,46 @@ public class FXMLController {
 
     @FXML
     void doActivation(ActionEvent event) {
+    	
+    	if(boxLingua.getValue() != null) {
+    		txtDaCorreggere.setDisable(false);
+    		txtCorretto.setDisable(false);
+    		SpellCheckBtn.setDisable(false);
+    		clearTxtBtn.setDisable(false);
+    		txtDaCorreggere.clear();
+    		txtCorretto.clear();
+    	} else {
+    		txtDaCorreggere.setDisable(true);
+    		txtCorretto.setDisable(true);
+    		SpellCheckBtn.setDisable(true);
+    		clearTxtBtn.setDisable(true);
+    		txtDaCorreggere.setText("Select language!");
+    	}
 
     }
 
     @FXML
     void doClearText(ActionEvent event) {
+    	txtDaCorreggere.clear();
+		txtCorretto.clear();
+		lblErrori.setText("Number of Errors:");
+		lblStato.setText("Spell Check Status:");
 
     }
 
     @FXML
     void doSpellScheck(ActionEvent event) {
-
+    	txtCorretto.clear();
+    	inputTextList = new LinkedList<String>();
+    	
+    	if(boxLingua.getValue() == null) {
+			txtDaCorreggere.setText("Seleziona una lingua!");
+			return;
+		}
+    	
+    	if(!dizionario.loadDictionary(boxLingua.getValue())) {
+    		// implementare
+    	}
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -67,5 +103,18 @@ public class FXMLController {
         assert clearTxtBtn != null : "fx:id=\"clearTxtBtn\" was not injected: check your FXML file 'Scene.fxml'.";
         assert lblStato != null : "fx:id=\"lblStato\" was not injected: check your FXML file 'Scene.fxml'.";
 
+    }
+    
+    public void setDictionary(Dictionary model) {
+    	
+    	txtDaCorreggere.setDisable(true);
+    	txtDaCorreggere.setText("Select language.");
+    	
+    	txtCorretto.setDisable(true);
+    	boxLingua.getItems().addAll("English", "Italian");
+    	
+    	SpellCheckBtn.setDisable(true);
+    	clearTxtBtn.setDisable(true);
+    	
     }
 }
--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -1,5 +1,70 @@
 package it.polito.tdp.spellchecker.model;
 
-public class Dictionary {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Dictionary {
+	
+	private List<String> dizionario;
+	private String language;
+	
+	public Dictionary() {
+		
+	}
+
+	public boolean loadDictionary(String language) {
+
+		if (dizionario!=null && this.language.equals(language)){
+			return true;
+		}
+		
+		dizionario = new ArrayList<String>();
+		this.language = language;
+		
+		try {
+
+			FileReader fr = new FileReader("src/main/resources/" + language + ".txt");
+			BufferedReader br = new BufferedReader(fr);
+			String word;
+
+			while ((word = br.readLine()) != null) {
+				dizionario.add(word.toLowerCase());
+			}
+
+			Collections.sort(dizionario);
+
+			br.close();
+			System.out.println("Dizionario " + language + " loaded. Found " + dizionario.size() + " words.");
+			
+			return true;
+
+		} catch (IOException e) {
+			System.err.println("Errore nella lettura del file");
+			return false;
+		}
+	}
+	
+	public List<RichWord> spellCheckText(List<String> inputTextList) {
+		
+		List<RichWord> parole = new ArrayList<>();
+		
+		for(String s : inputTextList) {
+			
+			RichWord rw = new RichWord(s);
+			if(dizionario.contains(s.toLowerCase()))
+				rw.setCorrect(true);
+			else 
+				rw.setCorrect(false);
+			
+			parole.add(rw);
+		}
+		
+		return parole;
+	}
+	
+	
 }
