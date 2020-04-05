04171005a23d34b8cd3e97a01803c9dfa70030df
gc <gc@gc-ThinkPad-T440p>
2020-04-05 13:14:10 +0200

    Interfaccia grafica completata

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -6,7 +6,12 @@ package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
@@ -16,10 +21,51 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="boxLingua"
+    private ComboBox<?> boxLingua; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtDaCorreggere"
+    private TextArea txtDaCorreggere; // Value injected by FXMLLoader
+
+    @FXML // fx:id="SpellCheckBtn"
+    private Button SpellCheckBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCorretto"
+    private TextArea txtCorretto; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblErrori"
+    private Label lblErrori; // Value injected by FXMLLoader
+
+    @FXML // fx:id="clearTxtBtn"
+    private Button clearTxtBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblStato"
+    private Label lblStato; // Value injected by FXMLLoader
+
+    @FXML
+    void doActivation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellScheck(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert boxLingua != null : "fx:id=\"boxLingua\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtDaCorreggere != null : "fx:id=\"txtDaCorreggere\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert SpellCheckBtn != null : "fx:id=\"SpellCheckBtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCorretto != null : "fx:id=\"txtCorretto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblErrori != null : "fx:id=\"lblErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert clearTxtBtn != null : "fx:id=\"clearTxtBtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblStato != null : "fx:id=\"lblStato\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
-
-
